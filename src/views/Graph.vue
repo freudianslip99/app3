a2857ff585ee3b135bd91e2163f087ac97067030
Alan B. Carpenter <uac@studentbox.ornl.gov>
2018-07-09 09:52:45 -0400

    update

--- src/views/Graph.vue
+++ src/views/Graph.vue
@@ -1,5 +1,31 @@
 <template>
+
   <div class="graph">
     <h1>This is a graph.</h1>
+    <v-layout row>
+        <v-flex xs12>
+            <svg id="visualization"  width="800" height="600"></svg>
+        </v-flex>
+</v-layout>
   </div>
+
 </template>
+
+
+
+
+<script>
+// @ is an alias to /src
+import plot from '@/components/plot.vue'
+import form from '@/components/form.vue'
+
+
+
+export default {
+  name: 'home',
+  components: {
+    plot,
+    form
+  }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -12,6 +12,8 @@
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
+
+
 export default {
   name: 'home',
   components: {
