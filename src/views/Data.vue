5e85d6ecbadd546e3161f291219be0573ce204ee
Alan B. Carpenter <uac@studentbox.ornl.gov>
2018-07-30 15:50:02 -0400

    added textReader

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -2,20 +2,16 @@
 <div>
 
   <div class="data">
-
-    <file-input v-model="filename" @formData="formData"></file-input>
-
-    <v-btn @click.native="uploadFiles"></v-btn>
+    <textarea rows="10" v-model="text"></textarea>
+    <br>
+    <text-reader @load="text = $event"></text-reader>
+    <br>
+    <br>
 
     <v-content>
 
       <v-container>
 
-        <v-flex xs12 class="text-xs-center text-sm-center text-md-center text-lg-center">
-
-          <input type='file' id='files' prepend-icon='attach_file' />
-
-        </v-flex>
 
       </v-container>
 
@@ -30,46 +26,17 @@
 </template>
 
 <script>
-import FileInput from '@/components/File-input.vue';
+import TextReader from '@/components/TextReader.vue';
 
 export default {
   name: 'data',
+  data: () => ({
+    text: '',
+  }),
   components: {
-    FileInput,
+    TextReader,
   },
-  methods: {
-    pickFile() {
-      console.log(this.$refs);
-      this.$refs.image.click();
-    },
 
-    onFilePicked(e) {
-      const files = e.target.files;
-      if (files[0] !== undefined) {
-        this.imageName = files[0].name;
-        if (this.imageName.lastIndexOf('.') <= 0) {
-          return;
-        }
-        const fr = new FileReader();
-
-        fr.readAsDataURL(files[0]);
-
-        fr.addEventListener('load', () => {
-          this.imageUrl = fr.result;
-          this.imageFile = files[0]; // this is an image file that can be sent to server...
-        });
-      } else {
-        this.imageName = '';
-        this.imageFile = '';
-        this.imageUrl = '';
-      }
-    },
-
-    uploadFiles() {
-      // your custom upload method
-      const form = this.formData;
-      console.log(form);
-    },
-  },
+  methods: {},
 };
 </script>
