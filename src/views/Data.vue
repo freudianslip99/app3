29dc98da9793d04461c7aafc0628266f4c84a732
Alan B. Carpenter <uac@studentbox.ornl.gov>
2018-08-02 11:24:04 -0400

    A

--- src/views/Data.vue
+++ src/views/Data.vue
@@ -11,15 +11,15 @@
     <v-content>
 
       <v-container>
-<form class="form-inline">
-    <div class="form-group">
-      <label for="files">Upload a CSV formatted file:</label>
-      <input type="file" id="files" class="form-control" accept=".csv" required />
-    </div>
-    <div class="form-group">
-      <button type="submit" id="submit-file" class="btn btn-primary">Upload File</button>
-    </div>
-  </form>
+        <form class="form-inline">
+          <div class="form-group">
+            <label for="files">Upload a CSV formatted file:</label>
+            <input type="file" id="files" class="form-control" accept=".csv" required />
+          </div>
+          <div class="form-group">
+            <button type="submit" id="submit-file" class="btn btn-primary">Upload File</button>
+          </div>
+        </form>
       </v-container>
 
     </v-content>
@@ -59,35 +59,35 @@ $('#submit-file').on('click', function(e) {
       delimiter: 'auto',
       complete: displayHTMLTable,
     },
-    /* before: function (files, inputElem) {
-      //console.log("Parsing file...", file);
+    before: function(files) {
+      console.log('Parsing file...', files);
     },
-    error: function (err, file) {
-      //console.log("ERROR:", err, file); */
-
+    error: function(err, file) {
+      console.log('ERROR:', err, file);
+    },
     complete: function() {
-      //console.log("Done with all files");
+      console.log('Done with all files');
     },
   });
-  let i = 0;
-  function displayHTMLTable(results) {
-    var table = "<table class='table'>";
-    var data = results.data;
-
-    for (i = 0; i < data.length; i++) {
-      table += '<tr>';
-      var row = data[i];
-      var cells = row.join(',').split(',');
-      let j = 0;
-      for (j = 0; j < cells.length; j++) {
-        table += '<td>';
-        table += cells[j];
-        table += '</th>';
-      }
-      table += '</tr>';
-    }
-    table += '</table>';
-    $('#parsed_csv_list').html(table);
-  }
 });
+
+function displayHTMLTable(results) {
+  var table = "<table class='table'>";
+  var data = results.data;
+  let i = 0;
+  for (i = 0; i < data.length; i++) {
+    table += '<tr>';
+    var row = data[i];
+    var cells = row.join(',').split(',');
+    let j = 0;
+    for (j = 0; j < cells.length; j++) {
+      table += '<td>';
+      table += cells[j];
+      table += '</th>';
+    }
+    table += '</tr>';
+  }
+  table += '</table>';
+  $('#parsed_csv_list').html(table);
+}
 </script>
